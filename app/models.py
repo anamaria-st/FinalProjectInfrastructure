d0bdaac830540d7d50df2c9f51803a322422d1c2
Ana Maria Alvarez Suarez <anamaria@stud.uni-obuda.hu>
2025-11-28 16:24:08 +0100

    update

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
-    # Email notifications
+    # Email notification
     notification_email = db.Column(db.String(120))
     email_notifications = db.Column(db.Boolean, default=False)
 
